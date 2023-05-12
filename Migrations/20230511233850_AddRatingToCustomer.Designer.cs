15fecf76e8f7cba672379eb79dc9b9408b7c4924
ruggeropaolobasile <ruggeropaolobasile@gmail.com>
2023-05-12 02:41:25 +0200

    first vesion

--- Migrations/20230511233850_AddRatingToCustomer.Designer.cs
+++ Migrations/20230511233850_AddRatingToCustomer.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using CRM1;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace CRM1.Migrations
 {
     [DbContext(typeof(CRMContext))]
-    partial class CRMContextModelSnapshot : ModelSnapshot
+    [Migration("20230511233850_AddRatingToCustomer")]
+    partial class AddRatingToCustomer
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
--- Models/Customer.cs
+++ Models/Customer.cs
@@ -1,16 +1,20 @@
 using System.ComponentModel.DataAnnotations;
 namespace CRM1.Models
 {
-public class Customer
-{
-    [Key]
-    public int Id { get; set; }
-    
-    [Required]
-    public string? Name { get; set; }
-    
-    public string? Email { get; set; }
-    
-    public string? PhoneNumber { get; set; }
-    // altre proprietà necessarie
-}}
+    public class Customer
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public string? Name { get; set; }
+
+        public string? Email { get; set; }
+
+        public string? PhoneNumber { get; set; }
+        // altre proprietà necessarie
+
+        [Range(1, 5)]
+        public int Rating { get; set; }
+    }
+}
